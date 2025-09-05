464bf290c096126c8dec8ecdd248fa99da88e285
Christian Leo Cereno <christiancereno424@gmail.com>
2025-09-05 21:22:58 +0800

    refactor: remove SocketProvider from main.jsx and delete the socketContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { SocketProvider } from "./context/SocketContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <SocketProvider>
-          <App />
-        </SocketProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
